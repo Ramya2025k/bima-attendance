bc8e8d2a3957af9ad7baf6b51d2696c026350c21
ramya <codezhii@gmail.com>
2026-08-17 10:44:08 +0530

    Switch to connection pooling for faster database access

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,20 @@
 ﻿import mysql from "mysql2/promise";
 
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL!,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: false,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 export async function getConnection() {
-  return mysql.createConnection({
-    uri: process.env.DATABASE_URL!,
-    ssl: {
-      minVersion: "TLSv1.2",
-      rejectUnauthorized: false,
-    },
-  });
+  const conn = await pool.getConnection();
+  conn.end = async () => {
+    conn.release();
+  };
+  return conn;
 }
